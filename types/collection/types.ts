a621473931f0abed45d21b5a1cb455ab6d1c903c
Benjamin Naesen <benjamin.naesen@studiohyperdrive.be>
2019-08-13 23:32:38 +0200

    feat: updated collection interfaces and types

--- types/collection/types.ts
+++ types/collection/types.ts
@@ -2,20 +2,73 @@ import { UserResponse } from '../user/types';
 import { CoreContentType } from '../core/content';
 
 export interface CollectionResponse {
-	fragments: CollectionFragment[];
-	description: string | null;
-	title: string;
-	is_public: boolean;
 	id: number;
-	lom_references: any[] | null;
-	type_id: number;
-	d_ownerid: number;
-	owner?: UserResponse;
+	collection_fragments: CollectionFragment[];
+	collection_permissions: CollectionPermission[];
 	created_at: string;
 	updated_at: string;
-	organisation_id: string | null;
-	mediamosa_id: string;
-	is_migrated_collection: boolean;
+	is_public: boolean;
+	publish_at: string | null;
+	depublish_at: string | null;
+	external_id: string | null;
+	description: string | null;
+	collection_fragment_ids: number[] | null;
+	label_redactie_id: number | null;
+	label_redactie: CollectionEditorsLabel | null;
+	lom_classification: string[] | null;
+	lom_context: string[] | null;
+	lom_typicalagerange: string[] | null;
+	lom_intendedenduserrole: string[] | null;
+	lom_keywords: string[] | null;
+	lom_languages: string[] | null;
+	organisation_id: string;
+	owner_id: string;
+	owner: UserResponse;
+	thumbnail_path: string | null;
+	title: string;
+	type_id: number;
+	type: CollectionType;
+}
+
+export interface CollectionType {
+	id: number;
+	label: string;
+}
+
+export interface CollectionPermission {
+	id: number;
+	collection_id: number;
+	created_at: string;
+	user_id: string;
+	permission_type: number;
+	updated_at: string;
+}
+
+export interface CollectionPermissionType {
+	id: number;
+	label: string;
+}
+
+export interface CollectionEditorsLabel {
+	id: number;
+	alt_label: string;
+	created_at: string;
+	label: string;
+	updated_at: string;
+}
+
+export interface CollectionFragment {
+	id: number;
+	collection_id: number;
+	created_at: string;
+	custom_description: string | null;
+	custom_title: string | null;
+	end_oc: number | null;
+	external_id: string;
+	position: number;
+	start_oc: number | null;
+	updated_at: string;
+	use_custom_fields: boolean;
 }
 
 export interface CollectionFragmentMigration {
@@ -27,117 +80,6 @@ export interface CollectionFragmentMigration {
 	updated_at: string;
 	start_oc: number | null;
 	end_oc: number | null;
-}
-
-export interface CollectionFragment {
-	id: number;
-	created_at: string;
-	updated_at: string;
-	type: 'BlockImageTitleTextButton',
-	fields: FieldInfo[] // Stored as jsonb in postgres
-}
-
-/**
- * Stores the values for each of the fields in a block
- *
- * Example of media fragment
- * fields: [
- * {
- *    name: 'external_id',
- *    label: 'Media id',
- *    editorType: 'none', // Do not show in editor screen
- *    value: '9p2w39hj6r',
- *    required: true,
- *  },
- * {
- *    name: 'custom_title',
- *    label: 'Eigen titel',
- *    editorType: 'string',
- *    value: 'KLAAR: artikel 13',
- *    required: false,
- *  },
- * {
- *    name: 'custom_description',
- *    label: 'Eigen beschrijving',
- *    editorType: 'textarea', // Do not show in editor screen
- *    value: 'Europa heeft onlangs de regels over het auteursrecht aangepast',
- *    required: false,
- *  },
- * {
- *    name: 'start_oc',
- *    label: 'Begin fragment',
- *    editorType: 'none',
- *    value: 15,
- *    required: false,
- *  },
- * {
- *    name: 'end_oc',
- *    label: 'Einde fragment',
- *    editorType: 'none',
- *    value: 25,
- *    required: false,
- *  },
- * ]
- *
- * Example of a BlockImageTitleTextButton block fragment
- * fields: [
- * {
- *    name: 'imageSource',
- *    label: 'Afbeelding',
- *    editorType: 'imageuploader',
- *    value: 'https://archief.viaa.be/ad87ac36b4f640dfb0fc57c26397b7.jpg',
- *    required: true,
- *  },
- * {
- *    name: 'imageDescription',
- *    label: 'Afbeelding beschrijving',
- *    editorType: 'string',
- *    value: '4 mensen die discusieren over auteursrecht in het europees parlement',
- *    required: false,
- *  },
- * {
- *    name: 'title',
- *    label: 'Titel',
- *    editorType: 'string',
- *    value: 'KLAAR: artikel 13',
- *    required: false,
- *  },
- * {
- *    name: 'text',
- *    label: 'Beschrijving',
- *    editorType: 'textarea',
- *    value: 'Europa heeft onlangs de regels over het auteursrecht aangepast',
- *    required: false,
- *  },
- * {
- *    name: 'buttonLabel',
- *    label: 'Knop label',
- *    editorType: 'string',
- *    value: 'Lees meer',
- *    required: false,
- *  },
- * {
- *    name: 'buttonLink',
- *    label: 'Knop link',
- *    editorType: 'string',
- *    value: '/item/141',
- *    required: false,
- *  },
- * {
- *    name: 'openInNewWindow',
- *    label: 'In nieuw vensten openen',
- *    editorType: 'toggle',
- *    value: true,
- *    required: true,
- *  },
- * ]
- */
-export interface FieldInfo {
-	name: string;
-	label: string;
-	editorType: 'string' | 'textarea' | 'imageuploader' | 'none'; // TODO add more
-	value: any;
-	required?: boolean;
 }
 
 export interface CollectionFragmentExternalId {
--- types/user/types.ts
+++ types/user/types.ts
@@ -1,13 +1,32 @@
 export interface UserResponse {
 	id: number;
-	user_id: string;
-	group_id: number;
-	org_id: string;
-	fn: string;
-	sn: string;
+	created_at: string;
+	email: string;
+	first_name: string;
+	last_name: string;
+	role_id: number | null;
+	role: UserRole | null;
+	pofile: UserProfile | null;
+	uid: string;
+	updated_at: string;
+}
+
+export interface UserRole {
+	id: number;
+	label: string;
+	name: string;
+}
+
+export interface UserProfile {
+	id: number;
 	alias: string | null;
 	alternative_email: string;
-	created_at: string;
-	updated_at: string;
 	avatar: string | null;
+	created_at: string;
+	fn: string;
+	group_id: number[];
+	org_id: string;
+	sn: string;
+	updated_at: string;
+	user_id: string;
 }
