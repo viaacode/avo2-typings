dcc5eb39883a8414511fd343850e39f6f3c18796
Bert Verhelst <verhelstbert@gmail.com>
2019-12-02 11:07:25 +0100

    feat(logging): implement logging format v4

--- types/event-logging/types.ts
+++ types/event-logging/types.ts
@@ -1,47 +1,43 @@
-export type SearchEventName = 'search';
-
-export type ItemEventName =
-	| 'play_item'
-	| 'bookmark_item'
-	| 'report_item'
-	| 'information_request_item'
-	| 'add_to_collection_item'
-	| 'view_item'
-	| 'publish_item'
-	| 'unpublish_item'
-	| 'edit_item'
-	| 'copy_item'
-	| 'share_item'
-	| 'create_item';
-
-export type AccountEventName =
-	| 'login_account'
-	| 'password_reset_account'
-	| 'modify_account'
-	| 'delete_account'
-	| 'block_account'
-	| 'unblock_account'
-	| 'register_account';
+export type EventAction =
+	| 'create'
+	| 'view'
+	| 'edit'
+	| 'delete'
+	| 'copy'
+	| 'play'
+	| 'bookmark'
+	| 'report'
+	| 'information_request'
+	| 'add_to_collection'
+	| 'publish'
+	| 'unpublish'
+	| 'share'
+	| 'login'
+	| 'password_reset'
+	| 'account'
+	| 'block'
+	| 'unblock'
+	| 'register'
+	| 'search';
 
 export interface ClientEvent {
-	name: EventName;
-	category: EventCategory;
-	event_subject: string; // entity doing the modification
-	event_subject_type: EventSubjectType;
-	event_object: string; // entity being modified
-	event_object_type: 'item' | 'collection' | 'bundle' | 'user';
-	event_message: any | null; // user played item xxx on avo
-	event_timestamp: string;
-	event_source: string; // eg: url when the event was triggered
+	action: EventAction;
+	subject: string; // entity doing the modification
+	subject_type: EventSubjectType;
+	object: string; // entity being modified
+	object_type: EventObjectType;
+	message: any | null; // user played item xxx on avo
+	occurred_at: string | null;
+	source_url: string; // eg: url when the event was triggered
 }
 
-export type EventName =
-	| SearchEventName
-	| ItemEventName
-	| AccountEventName;
+export type EventSubjectType = 'user_uuid' | 'anonymous_user' | 'system';
 
-export type EventCategory = 'item' | 'user' | 'search';
-
-export type EventSubjectType = 'user_uuid' | 'system';
-
-export type EventObjectType = 'bundles' | 'collections' | 'avo_item_pid' | 'archief_item_pid' | 'user_uuid';
+export type EventObjectType =
+	| 'bundels'
+	| 'collections'
+	| 'avo_item_pid'
+	| 'archief_item_pid'
+	| 'user_uuid'
+	| 'anonymous_user'
+	| 'system';
