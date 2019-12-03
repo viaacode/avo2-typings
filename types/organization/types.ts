72c6252d346155c3c4b2fe0c77b928a2a75c0f09
Benjamin Naesen <benjamin@naesen.be>
2019-12-03 20:34:32 +0100

    feat: add organization types

---
+++ types/organization/types.ts
@@ -0,0 +1,27 @@
+export interface OrganizationContactInfo {
+	phone?: string;
+	website?: string;
+	email?: string;
+	logoUrl?: string;
+	form_url?: string;
+}
+
+export interface OrganizationData {
+	or_id: string;
+	cp_name: string;
+	category?: string;
+	sector?: string;
+	cp_name_catpro?: string;
+	description?: string;
+	contact_information: OrganizationContactInfo;
+	accountmanager?: string;
+}
+
+export interface OrganizationSchema {
+	or_id: string;
+	name: string;
+	logo_url?: string;
+	description?: string;
+	website?: string;
+	data: OrganizationData;
+}
