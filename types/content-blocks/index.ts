346e12daf453ef37d8a2f671160af97abb7acfd5
Andry Charlier <andry.charlier@studiohyperdrive.be>
2020-01-16 13:38:31 +0100

    fix: content-block position must be a number

--- types/content-blocks/index.ts
+++ types/content-blocks/index.ts
@@ -2,7 +2,7 @@ export interface ContentBlockSchema {
 	id: number;
 	content_id: number;
 	variables: { [key: string]: any } | any[] | null;
-	position: number | null;
+	position: number;
 	created_at: string;
 	updated_at: string;
 	content_block_type: string;
