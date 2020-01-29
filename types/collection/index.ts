44eac6a33c2ba02a377c6d7925e12a068e61beff
Bert Verhelst <verhelstbert@gmail.com>
2020-01-29 10:08:10 +0100

    fix(collection): update collection types to match db changes

--- types/collection/index.ts
+++ types/collection/index.ts
@@ -3,18 +3,18 @@ import { ContentTypeSchema } from '../core/content';
 import { ItemSchema } from '../item';
 
 export interface CollectionSchema {
-	id: number;
+	id: string;
+	avo1_id: string | null;
 	collection_fragments: CollectionFragment[];
-	collection_permissions: CollectionPermission[];
 	created_at: string;
 	updated_at: string;
 	is_public: boolean;
+	is_deleted: boolean;
 	publish_at: string | null;
+	published_at: string | null;
 	depublish_at: string | null;
 	external_id: string | null;
 	description: string | null;
-	collection_fragment_ids: number[] | null;
-	item_meta?: ItemSchema;
 	label_redactie_id: number | null;
 	label_redactie: CollectionEditorsLabel | null;
 	lom_classification: string[] | null;
@@ -25,29 +25,17 @@ export interface CollectionSchema {
 	lom_languages: string[] | null;
 	organisation_id: string;
 	owner_profile_id: string;
-	profile: UserProfile;
+	profile: UserProfile | null;
 	note: string | null;
+	redaction: boolean | null;
+	redaction_at: string | null;
 	thumbnail_path: string | null;
 	title: string;
 	type_id: number;
 	type: {
 		id: number;
 		label: string;
-	};
-}
-
-export interface CollectionPermission {
-	id: number;
-	collection_id: number;
-	created_at: string;
-	user_id: string;
-	permission_type: number;
-	updated_at: string;
-}
-
-export interface CollectionPermissionType {
-	id: number;
-	label: string;
+	} | null;
 }
 
 export interface CollectionEditorsLabel {
@@ -60,7 +48,8 @@ export interface CollectionEditorsLabel {
 
 export interface CollectionFragment {
 	id: number;
-	collection_id: number;
+	collection_id: number; // Deprecated, use collection_uuid instead
+	collection_uuid: number;
 	created_at: string;
 	custom_description: string | null;
 	custom_title: string | null;
@@ -71,6 +60,8 @@ export interface CollectionFragment {
 	updated_at: string;
 	use_custom_fields: boolean;
 	item_meta?: ItemSchema;
+	is_media_fragment: boolean; // Deprecated, use type instead
+	type: 'ITEM' | 'TEXT' | 'COLLECTION'; // A collection can contain ITEM or TEXT fragments, a bundle can contain COLLECTION fragments
 }
 
 export interface CollectionFragmentExternalId {
