adeeac9f5289443f79f49c412508ee120a9f2089
Bert Verhelst <verhelstbert@gmail.com>
2020-03-13 14:36:35 +0100

    feat(content-labels): add types for labels and label link table

--- types/content/index.ts
+++ types/content/index.ts
@@ -3,6 +3,25 @@ import { ContentBlockSchema } from '../content-blocks';
 
 export type ContentWidthSchema = 'REGULAR' | 'LARGE' | 'MEDIUM';
 
+export interface ContentLabelSchema {
+	id: number;
+	label: string;
+	content_type: string;
+	created_at: string;
+	updated_at: string;
+	content_content_labels: ContentLabelLinkSchema[];
+}
+
+export interface ContentLabelLinkSchema {
+	id: number;
+	content_id: number;
+	label_id: number;
+	created_at: string;
+	updated_at: string;
+	content_label: ContentLabelSchema[];
+	content: ContentSchema[];
+}
+
 export interface ContentSchema {
 	id: number;
 	title: string;
@@ -20,5 +39,6 @@ export interface ContentSchema {
 	profile: UserProfile;
 	user_profile_id: string | null;
 	user_group_ids: number[] | null;
-	contentBlockssBycontentId: ContentBlockSchema[]
+	contentBlockssBycontentId: ContentBlockSchema[];
+	labels: ContentLabelSchema[]
 }
