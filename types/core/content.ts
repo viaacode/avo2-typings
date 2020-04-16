1555d5035187ab83c79584871e33cb1b5867c26a
Bert Verhelst <verhelstbert@gmail.com>
2020-04-16 10:39:49 +0200

    fix(content-picker): add ANCHOR_LINK type

--- types/core/content.ts
+++ types/core/content.ts
@@ -1,6 +1,6 @@
 export type ContentTypeSchema = 'collectie' | 'video' | 'audio' | 'bundel';
 
-export type ContentPickerTypeSchema = 'CONTENT_PAGE' | 'COLLECTION' | 'ITEM' | 'BUNDLE' | 'DROPDOWN' | 'INTERNAL_LINK' | 'EXTERNAL_LINK' | 'SEARCH_QUERY' | 'PROJECTS';
+export type ContentPickerTypeSchema = 'CONTENT_PAGE' | 'COLLECTION' | 'ITEM' | 'BUNDLE' | 'DROPDOWN' | 'INTERNAL_LINK' | 'EXTERNAL_LINK' | 'SEARCH_QUERY' | 'PROJECTS' | 'ANCHOR_LINK';
 
 export type MediaTypeSchema = {
 	id: number;
