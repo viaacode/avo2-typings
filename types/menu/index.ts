a399b9eca335ed4814fd0dd7a10168f49f9938d8
Benjamin Naesen <benjamin@naesen.be>
2020-04-20 11:38:33 +0200

    feat(menu): tooltip

--- types/menu/index.ts
+++ types/menu/index.ts
@@ -2,15 +2,16 @@ import { ContentPickerTypeSchema } from '../core/content';
 
 export interface MenuSchema {
 	id: number;
-	label: string;
+	label?: string;
 	icon_name: string;
-	description: string | null;
+	description?: string | null;
 	user_group_ids: number[] | null;
 	content_type: ContentPickerTypeSchema | null;
 	content_path: string | number | null;
 	link_target: '_blank' | '_self' | null;
 	position: number;
-	placement: string;
+	placement?: string;
 	created_at: string;
 	updated_at: string;
+	tooltip?: string;
 }
