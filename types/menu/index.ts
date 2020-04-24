fdfc19ccfd9c39ab12150f78d4c52c9f6c97356d
Benjamin Naesen <benjamin@naesen.be>
2020-04-24 15:11:04 +0200

    fix(menu): remove ?

--- types/menu/index.ts
+++ types/menu/index.ts
@@ -2,16 +2,16 @@ import { ContentPickerTypeSchema } from '../core/content';
 
 export interface MenuSchema {
 	id: number;
-	label?: string | null;
+	label: string | null;
 	icon_name: string;
-	description?: string | null;
+	description: string | null;
 	user_group_ids: number[] | null;
 	content_type: ContentPickerTypeSchema | null;
 	content_path: string | number | null;
 	link_target: '_blank' | '_self' | null;
 	position: number;
-	placement?: string | null;
+	placement: string | null;
 	created_at: string;
 	updated_at: string;
-	tooltip?: string | null;
+	tooltip: string | null;
 }
