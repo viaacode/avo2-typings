0030522b973862f274a324a52cee972d148df630
Benjamin Naesen <benjamin@naesen.be>
2020-04-24 10:46:25 +0200

    fix(menu): add | null

--- types/menu/index.ts
+++ types/menu/index.ts
@@ -2,7 +2,7 @@ import { ContentPickerTypeSchema } from '../core/content';
 
 export interface MenuSchema {
 	id: number;
-	label?: string;
+	label?: string | null;
 	icon_name: string;
 	description?: string | null;
 	user_group_ids: number[] | null;
@@ -10,8 +10,8 @@ export interface MenuSchema {
 	content_path: string | number | null;
 	link_target: '_blank' | '_self' | null;
 	position: number;
-	placement?: string;
+	placement?: string | null;
 	created_at: string;
 	updated_at: string;
-	tooltip?: string;
+	tooltip?: string | null;
 }
