c402e5a6a3f706f72ca88d94226b9e264e8601f5
Benjamin Naesen <benjamin@naesen.be>
2021-02-10 11:57:32 +0100

    feat(users): add (un)blocked_at

--- types/user/index.d.ts
+++ types/user/index.d.ts
@@ -16,10 +16,12 @@ export interface UserSchema {
 	expires_at: string | null;
 	mail: string;
 	is_blocked: boolean | null;
+	blocked_at: string;
+	unblocked_at: string;
 	idpmaps: IdpTypeSchema[];
 	idpmapObjects: Array<{
-		idp: IdpTypeSchema,
-		idp_user_id: string
+		idp: IdpTypeSchema;
+		idp_user_id: string;
 	}>;
 }
 
@@ -38,7 +40,10 @@ export interface UserProfile {
 	permissions: string[] | undefined;
 	educationLevels: string[];
 	subjects: string[];
-	organizations: Array<{ organizationName: string, unitAddress?: string }>;
+	organizations: Array<{
+		organizationName: string;
+		unitAddress?: string;
+	}>;
 	company_id: string | null;
 	organisation: OrganizationSchema | null;
 	is_exception: boolean;
