246233e892138373f0b71393395f70bd56b7efc1
Bert Verhelst <bert.verhelst@studiohyperdrive.be>
2022-09-06 11:32:04 +0200

    feat(avo-2136): split idp VLAAMSEOVERHEID for the 2 ids

--- types/auth/index.d.ts
+++ types/auth/index.d.ts
@@ -2,16 +2,24 @@ import { UserSchema } from '../user';
 
 export type LoginResponseSchema =
 	| {
-			message: 'LOGGED_IN';
-			userInfo: UserSchema;
-			acceptedConditions: boolean;
-			sessionExpiresAt: string;
-	  }
+	message: 'LOGGED_IN';
+	userInfo: UserSchema;
+	acceptedConditions: boolean;
+	sessionExpiresAt: string;
+}
 	| {
-			message: 'LOGGED_OUT';
-	  };
+	message: 'LOGGED_OUT';
+};
 
-export type IdpTypeSchema = 'HETARCHIEF' | 'SMARTSCHOOL' | 'KLASCEMENT' | 'VLAAMSEOVERHEID';
+export type IdpTypeSchema =
+	| 'HETARCHIEF'
+	| 'SMARTSCHOOL'
+	| 'KLASCEMENT'
+	| 'VLAAMSEOVERHEID__SUB_ID'
+	| 'VLAAMSEOVERHEID__ACCOUNT_ID'
+
+	// Deprecated, use VLAAMSEOVERHEID__SUB_ID instead
+	| 'VLAAMSEOVERHEID';
 
 export type LoginMessageSchema = 'LOGGED_IN' | 'LOGGED_OUT';
 
