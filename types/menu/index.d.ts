f28aed4322e9883e8be497860f6369611174bcd2
Bert Verhelst <bert.verhelst@studiohyperdrive.be>
2022-10-18 14:52:41 +0200

    fix(avo-2209): make some of the menu properties optional

--- types/menu/index.d.ts
+++ types/menu/index.d.ts
@@ -4,10 +4,10 @@ export interface MenuSchema {
 	id: number;
 	label: string | null;
 	icon_name: string;
-	description: string | null;
-	user_group_ids: number[] | null;
+	description?: string | null;
+	user_group_ids?: number[] | null;
 	content_type: ContentPickerTypeSchema | null;
-	content_path: string | number | null;
+	content_path: string | null;
 	link_target: '_blank' | '_self' | null;
 	position: number;
 	placement: string | null;
