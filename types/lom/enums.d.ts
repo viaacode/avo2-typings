aef142211abd82f0c705f75b15cdc650ac74a7eb
SHDBramVandenbussche <bram.vandenbussche@studiohyperdrive.be>
2023-06-06 15:56:26 +0200

    feature(avo-2638): Add lom types and enum

---
+++ types/lom/enums.d.ts
@@ -0,0 +1,5 @@
+export declare enum LomSchemeTypeEnum {
+    subject = "https://w3id.org/onderwijs-vlaanderen/id/vak",
+    structure = "https://w3id.org/onderwijs-vlaanderen/id/structuur",
+    theme = "https://data.hetarchief.be/id/onderwijs/thema"
+}
