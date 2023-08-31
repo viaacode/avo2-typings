559e131e740e1f8e3f852abb94ed7f7542d1d1db
Bert Verhelst <verhelstbert@gmail.com>
2023-08-31 18:32:17 +0200

    fix(avo-2746): add enum for asset type
    https://meemoo.atlassian.net/browse/AVO-2746

--- types/enums.d.ts
+++ types/enums.d.ts
@@ -1,4 +1,5 @@
 export * from './permissions/enums';
+export * from './assets/enums';
 export * from './core/enums';
 export * from './lom/enums';
 export * from './shared/shared-with-colluegue-type-enum';
